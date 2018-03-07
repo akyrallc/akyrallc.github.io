1d2af5a33391da90a928e364bdf16e3f12af4763
akyrallc <36460589+akyrallc@users.noreply.github.com>
2018-03-07 13:42:09 -0800

    Added Pic space
    Pic space

--- data/posts.js
+++ data/posts.js
@@ -1,23 +1,28 @@
 var posts = [{
         "id": "5",
         "description": "Ask yourself, WHY? Trust the plan.",
-        "currentdate": "03/07/2018"
+        "currentdate": "03/07/2018",
+        "img": null
     },
     {
         "id": "4",
         "description": "Dark to LIGHT. The choice, to KNOW, will be yours.",
-        "currentdate": "03/06/2018"
+        "currentdate": "03/06/2018",
+        "img": null
     }, {
         "id": "3",
         "description": "Up is Down. Left is Right.",
-        "currentdate": "03/05/2018"
+        "currentdate": "03/05/2018",
+        "img": null
     }, {
         "id": "2",
         "description": "Future Proves Past.",
-        "currentdate": "03/02/2018"
+        "currentdate": "03/02/2018",
+        "img": null
     }, {
         "id": "1",
         "description": "The Hunters become the hunted. 2018 will be glorious!!",
-        "currentdate": "03/02/2018"
+        "currentdate": "03/02/2018",
+        "img": null
     }
 ]
---
+++ images/readme.txt
@@ -0,0 +1,1 @@
+Add all images in this folder
