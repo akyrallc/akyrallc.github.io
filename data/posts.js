4029416fbda2aff4ac60cd97a9a5e3e14ae7cc82
akyrallc <36460589+akyrallc@users.noreply.github.com>
2018-03-07 13:16:34 -0800

    added folder
    Folder

---
+++ data/posts.js
@@ -0,0 +1,17 @@
+var posts = [{
+    "id": "4",
+    "description": "Dark to LIGHT. The choice, to KNOW, will be yours.",
+    "currentdate": "03/06/2018"
+}, {
+    "id": "3",
+    "description": "Up is Down. Left is Right.",
+    "currentdate": "03/05/2018"
+}, {
+    "id": "2",
+    "description": "Future Proves Past.",
+    "currentdate": "03/02/2018"
+}, {
+    "id": "1",
+    "description": "The Hunters become the hunted. 2018 will be glorious!!",
+    "currentdate": "03/02/2018"
+}]
