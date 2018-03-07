40d9d1fa39076b502c08f22b297212565a87df26
akyrallc <36460589+akyrallc@users.noreply.github.com>
2018-03-07 13:20:57 -0800

    Wed
    wed

--- data/posts.js
+++ data/posts.js
@@ -1,17 +1,23 @@
 var posts = [{
-    "id": "4",
-    "description": "Dark to LIGHT. The choice, to KNOW, will be yours.",
-    "currentdate": "03/06/2018"
-}, {
-    "id": "3",
-    "description": "Up is Down. Left is Right.",
-    "currentdate": "03/05/2018"
-}, {
-    "id": "2",
-    "description": "Future Proves Past.",
-    "currentdate": "03/02/2018"
-}, {
-    "id": "1",
-    "description": "The Hunters become the hunted. 2018 will be glorious!!",
-    "currentdate": "03/02/2018"
-}]
+        "id": "5",
+        "description": "Ask yourself, WHY? Trust the plan.",
+        "currentdate": "03/07/2018"
+    },
+    {
+        "id": "4",
+        "description": "Dark to LIGHT. The choice, to KNOW, will be yours.",
+        "currentdate": "03/06/2018"
+    }, {
+        "id": "3",
+        "description": "Up is Down. Left is Right.",
+        "currentdate": "03/05/2018"
+    }, {
+        "id": "2",
+        "description": "Future Proves Past.",
+        "currentdate": "03/02/2018"
+    }, {
+        "id": "1",
+        "description": "The Hunters become the hunted. 2018 will be glorious!!",
+        "currentdate": "03/02/2018"
+    }
+]
