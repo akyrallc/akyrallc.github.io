d2a3c98339028b330cd1d5b37bcc20e04b4824ff
Rachel <rachel.neubauer74@gmail.com>
2018-03-08 07:33:00 -0800

    Rachel First Code
    First Code!

--- data/posts.js
+++ data/posts.js
@@ -1,4 +1,10 @@
 var posts = [
+    {
+        "id": "6",
+        "description": "Expand your thinking. Moves and countermoves.Just because you can't see it, doesn't mean big things aren't happening.",
+        "currentdate": "03/08/2018",
+        "img": null
+    },
     
     {
         "id": "5",
