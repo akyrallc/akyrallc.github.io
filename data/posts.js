d6c7be6799c6ee109ca4146f8faf690865dea65e
Rachel <rachel.neubauer74@gmail.com>
2018-03-09 07:28:31 -0800

    Friday
    Friday

--- data/posts.js
+++ data/posts.js
@@ -1,5 +1,11 @@
 var posts = [
     {
+        "id": "7",
+        "description": "Do you know that we know? Nobody playing the game gets a free pass. Nobody.",
+        "currentdate": "03/09/2018",
+        "img": null
+    },
+    {
         "id": "6",
         "description": "Expand your thinking. Moves and countermoves. Just because you can't see it, doesn't mean big things aren't happening.",
         "currentdate": "03/08/2018",
