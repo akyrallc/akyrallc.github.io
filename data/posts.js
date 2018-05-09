18d4019151dcb2756407f65e8a92f04a5ad40e39
akyrallc <36460589+akyrallc@users.noreply.github.com>
2018-05-09 09:06:07 -0700

    Test
    Test

--- data/posts.js
+++ data/posts.js
@@ -1,6 +1,6 @@
 var posts = [{
         "id": "50",
-        "description": "Lightning",
+        "description": "",
         "currentdate": "05/10/2018",
         "img": "./data/image/IMG_1509.JPG"
     }, {
